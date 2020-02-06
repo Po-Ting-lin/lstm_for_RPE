b44c91848108226e9ceb72e9f17b4609e9e92ab6
BT <c1c4c5c6@gmail.com>
2020-02-06 16:45:30 +0800

    lr

--- lstm/main.py
+++ lstm/main.py
@@ -16,46 +16,59 @@ X, y = data_generator()
 config_dict = {"input shape": X.shape,
                "units": 20,
                "dropout": 0.5,
-               "batch size": 4,
+               "batch size": 10,
                "epochs": 50,
+               "learning rate": 0.001,
                "reduce learning rate": 0}
 
+auc_lr = []
+for lr in [0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1]:
 
-# training
-auc_buf = []
-kf = KFold(n_splits=3, random_state=None, shuffle=True)
-for train_index, test_index in kf.split(X):
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    print(lr)
+    config_dict["learning rate"] = lr
 
-    model = bulid_model(config_dict)
+    # training
+    auc_buf = []
+    kf = KFold(n_splits=3, random_state=None, shuffle=True)
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
 
-
-    plot_model(model, to_file='model.png', show_shapes=True)
-
-    history = model.fit(X_train, y_train,
-                        validation_data=(X_test, y_test),
-                        epochs=config_dict["epochs"],
-                        batch_size=config_dict['batch size'],
-                        callbacks=getCallback(config_dict["reduce learning rate"]),
-                        verbose=0)
-
-    model.summary()
-
-    model = load_model(DEEP_LSTM_ROOT + '\\model_0202_lstm_test.h5')
-    predictions = model.predict(X_test)
-
-    area_under_curve = evaluate_model_performance(predictions, y_test, plot_mode=True)
-    preds = evaluation(predictions, y_test)
-    auc_buf.append(area_under_curve)
-
-    # for g in range(5):
-    #     random.seed(int(time.time() * 100) % 100)
-    #     idx = random.randint(0, len(y_test)-1)
-    #     plot_time_series_data(X_test[idx], y_test[idx], preds[idx])
-
-    show_final_history(history)
+        model = bulid_model(config_dict)
 
 
-k_auc = np.mean(auc_buf)
-print("AUC: {:.3f} ".format(k_auc))
+        plot_model(model, to_file='model.png', show_shapes=True)
+
+        history = model.fit(X_train, y_train,
+                            validation_data=(X_test, y_test),
+                            epochs=config_dict["epochs"],
+                            batch_size=config_dict['batch size'],
+                            callbacks=getCallback(config_dict["reduce learning rate"]),
+                            verbose=0)
+
+        model.summary()
+
+        model = load_model(DEEP_LSTM_ROOT + '\\model_0202_lstm_test.h5')
+        predictions = model.predict(X_test)
+
+        area_under_curve = evaluate_model_performance(predictions, y_test, plot_mode=True)
+        preds = evaluation(predictions, y_test)
+        auc_buf.append(area_under_curve)
+
+        # for g in range(5):
+        #     random.seed(int(time.time() * 100) % 100)
+        #     idx = random.randint(0, len(y_test)-1)
+        #     plot_time_series_data(X_test[idx], y_test[idx], preds[idx])
+
+        show_final_history(history)
+
+
+    k_auc = np.mean(auc_buf)
+    print("AUC: {:.3f} ".format(k_auc))
+    auc_lr.append(k_auc)
+
+
+plt.figure()
+plt.plot([0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1],[0.914, 0.968, 0.972, 0.984, 0.966, 0.986, 0.981, 0.94, 0.813])
+plt.xticks([0.0001,0.0005,0.001,0.005,0.01,0.05,0.1,0.5,1])
+plt.show()
--- lstm/model_gen.py
+++ lstm/model_gen.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense, Conv1D, LSTM, Dropout, Bidirectional
+from keras.optimizers import Adam
 
 
 def bulid_model(config):
@@ -9,6 +10,9 @@ def bulid_model(config):
     model.add(Bidirectional(LSTM(config['units'], input_shape=(num_time_steps, num_features), dropout=config['dropout'])))
     model.add(Dropout(config['dropout']))
     model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    a = Adam(lr=config["learning rate"], beta_1=0.9, beta_2=0.999, amsgrad=False)
+
+    model.compile(loss='binary_crossentropy', optimizer=a, metrics=['accuracy'])
     return model
 
